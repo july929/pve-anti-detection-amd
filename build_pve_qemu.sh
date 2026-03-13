37f8659982f381aee1b05e351ae0de7b136ee277
July's <37677092+july929@users.noreply.github.com>
2026-03-13 11:35:35 +0800

    Update build_pve_qemu.sh

--- build_pve_qemu.sh
+++ build_pve_qemu.sh
@@ -4,10 +4,8 @@ ls
 df -h
 git clone git://git.proxmox.com/git/pve-qemu.git
 cd pve-qemu
-# pve8 9.2.0-7
-#git reset --hard 245689b9ae4120994de29b71595ea58abac06f3c
-# pve9 10.1.2-5
-git reset --hard de7f8fe356c7b1d346c3c15c971f7a0dcd11e70e
+# pve9 10.1.2-7
+git reset --hard a7c7a6b2b1aa75360d914b252dfcb05506ce590b
 sudo apt install devscripts -y
 yes | sudo mk-build-deps --install
 git submodule update --init --recursive
